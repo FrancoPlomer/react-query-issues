9c20348affd076258e04e7c867279a0ab9cb257c
FrancoPlomer <franko.plomer@gmail.com>
2024-03-05 11:03:18 -0300

    optimizando peticiones

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,9 +1,13 @@
 import { FC             } from 'react';
 import { Issue, State   } from '../interfaces';
+import { useNavigate    } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
+import { 
+    getIssueInfo, getIssueComments   
+} from '../hooks/useIssue';
 import { 
     FiCheckCircle, FiInfo, FiMessageSquare 
 } from 'react-icons/fi';
-import { useNavigate } from 'react-router-dom';
 
 
 interface Props {
@@ -12,12 +16,38 @@ interface Props {
 
 export const IssueItem: FC<Props> = ({ issue }) => {
 
-    const navigate = useNavigate();
+    const navigate      = useNavigate();
+    const queryClient   = useQueryClient();
+
+    //Solicita la informacion sobre el issue y sus coments de manera rapida ante un evento
+   
+    // const prefetchData = () => {
+
+    //     queryClient.prefetchQuery({
+    //         queryKey: [ 'issue', issue.number ],
+    //         queryFn: () => getIssueInfo( issue.number ),
+    //     })
+    //     queryClient.prefetchQuery({
+    //         queryKey: [ 'issue', issue.number, 'comments' ],
+    //         queryFn: () => getIssueComments( issue.number ),
+    //     })
+    // }
+
+    const preSetData = () => {
+        queryClient.setQueryData(
+            [ 'issue', issue.number ], 
+            issue,
+            {
+                updatedAt: new Date().getTime() + 10000,
+            }
+        )        
+    }
 
     return (
         <div 
         className="card mb-2 issue"
         onClick={ () => navigate( `/issues/issue/${ issue.number }` ) }
+        onMouseEnter={ preSetData }
         >
             <div className="card-body d-flex align-items-center">
                 {
